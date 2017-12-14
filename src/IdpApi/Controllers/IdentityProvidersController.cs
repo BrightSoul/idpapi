b4776c76bd7272502517ced8c2a1f8004af3dada
Moreno Gentili <brightsoul@tiscali.it>
2017-12-14 09:13:43 +0100

    First commit

---
+++ src/IdpApi/Controllers/IdentityProvidersController.cs
@@ -0,0 +1,54 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using IdpApi.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdpApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class IdentityProvidersController : Controller
+    {
+        private readonly IHostingEnvironment env;
+        public IdentityProvidersController(IHostingEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [HttpGet]
+        public List<IdentityProvider> GetIdentityProviders()
+        {
+            var identityProviders = new List<IdentityProvider>();
+            using (var reader = System.IO.File.OpenText(Path.Combine(env.ContentRootPath, "data.csv")))
+            {
+                var config = new CsvConfiguration { HasHeaderRecord = false, Delimiter = "\t", Quote = '"', QuoteAllFields = true, };
+
+                var csv = new CsvReader(reader, config);
+                while (csv.Read())
+                {
+                    var name = csv.GetField<string>(0);
+                    var metadataUrl = csv.GetField<string>(1);
+                    var certificate = csv.GetField<string>(2).Replace(" ", "").Replace("\r", "").Replace("\t", "");
+                    var attributes = JsonConvert.DeserializeObject<AttributesMap>(csv.GetField<string>(3));
+                    identityProviders.Add(new IdentityProvider
+                    {
+                         metadata_url = metadataUrl,
+                         cert = certificate,
+                         entityID = metadataUrl,
+                         logo = $"{Request.Scheme}://{Request.Host}/logo.png",
+                         name = name,
+                         attributes_map = attributes
+                    });
+                }
+            }
+
+            return identityProviders;
+        }
+    }
+}
---
+++ src/IdpApi/Model/IdentityProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdpApi.Model
+{
+
+    public class IdentityProvider
+    {
+        public IdentityProvider()
+        {
+            attributes_map = new AttributesMap();
+        }
+        public string cert { get; set; }
+        public AttributesMap attributes_map { get; set; }
+        public string entityID { get; set; }
+        public string metadata_url { get; set; }
+        public string name { get; set; }
+        public string logo { get; set; }
+    }
+
+    public class AttributesMap
+    {
+        public string email { get; set; }
+        public string username { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+    }
+
+}
